d6fabe770af75b6138ae4c7f396a86b2774f4565
Asma <user@DESKTOP-KIPN1TO>
2020-03-17 08:59:13 +0600

    ES6 done

--- inheritance.js
+++ inheritance.js
@@ -1,1 +1,18 @@
+ class Parents{
+     constructor(){
+         this.fathername = "Schwerznegger"
+     }
+ }
+class Child extends Parents{
+    constructor(name){
+        super();
+        this.name = name ;
+    }
 
+    getFullName(){
+        return this.name + " "+ this.fathername;
+    }
+}
+
+const baby= new Child("Arnold");
+console.log(baby.getFullName());
